66adc7163c62f815582bc87dc164cefe62e1a2e5
Lance Russell <lancebryanrussell@gmail.com>
2020-06-14 12:23:48 -0400

    added database function

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,16 +9,21 @@ class Header extends React.Component {
   };
 
   render () {
+    let width = 0;
+    if(this.state.active){
+      width = 300;
+    }
+
     return (
       <div id="header">
         <Burger
           onClick={() => this.setState({ active: !this.state.active })}
           active={this.state.active}
+          color={this.state.active ? 'white' : '#1D5171'}
           burger="stand"
-          hoverOpacity={0.8}
+          hoverOpacity={0.9}
         />
-      {this.state.active && 
-        <div id="sidenav">
+      {<div id="sidenav" style={{width}}>
           <Link to="/">Home</Link>
           <Link to="/add-event">Add an Event</Link>
           <Link to="/login">Login</Link>
